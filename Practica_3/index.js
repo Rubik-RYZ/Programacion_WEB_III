9ef4bf31055c0619acb4cd00ae19295efa3f6284
Rubik-RYZ <bframirezy567@gmail.com>
2025-04-03 02:05:28 -0400

    _

--- Practica_3/index.js
+++ Practica_3/index.js
@@ -12,20 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'cursosdb'
-});
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
 
 const cursosRoutes = require('./controllers/cursosController');
 
